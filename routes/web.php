f3f0e9107b76772e21d57dfaec3798243d4235a9
NgTienDat <dat.nti.64cntt@ntu.edu.vn>
2026-03-07 22:56:42 +0700

    dang ky,dang nhap

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,20 @@
 <?php
-use App\Http\Controllers\Admin\DashboardController;
+
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\HomeController;
-Route::get('/', [HomeController::class, 'index'])->name('home');
-Route::prefix('admin')
-    ->name('admin.')
-    //->middleware(['auth']) // sau này thêm isAdmin
-    ->group(function () {
 
-    Route::get('/', [DashboardController::class, 'index'])
-        ->name('dashboard');
-    Route::resource('categories', \App\Http\Controllers\Admin\CategoryController::class);
-    Route::resource('brands', \App\Http\Controllers\Admin\BrandController::class);
-    Route::resource('products', \App\Http\Controllers\Admin\ProductController::class);
-    Route::resource('inventory', \App\Http\Controllers\Admin\InventoryController::class)->only(['index', 'create', 'store']);
-
+Route::get('/', function () {
+    return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
